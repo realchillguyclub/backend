fbeac024c73ad753d9d706062b6757945883bac9
bbbang105 <2018111366@dgu.ac.kr>
2025-02-02 16:53:36 +0900

    [feat] : 테스트 시 오류 방지를 위해 클래스를 지정한다

--- src/test/java/com/rcgc/illdan/IlldanApplicationTests.java
+++ src/test/java/com/rcgc/illdan/IlldanApplicationTests.java
@@ -3,7 +3,7 @@ package com.rcgc.illdan;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = IlldanApplicationTests.class)
 class IlldanApplicationTests {
 
     @Test
