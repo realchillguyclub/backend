b40c4a2c5afe1f3d01c48822bd193e68b0b493a6
신재웅 (JaeWoong Shin) <tlswodnd4316@gmail.com>
2025-10-27 23:22:58 +0900

    ILLDAN-185 [fix]: 서비스 레이어에서의 Transactional을 추후 Redis 연산 분리 후 적용하기 위해 레포지토리에 Transactional을 적용한다

--- src/main/java/server/poptato/user/infra/repository/JpaMobileRepository.java
+++ src/main/java/server/poptato/user/infra/repository/JpaMobileRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import server.poptato.user.domain.entity.Mobile;
 import server.poptato.user.domain.repository.MobileRepository;
 
@@ -13,9 +14,11 @@ import java.util.List;
 public interface JpaMobileRepository extends MobileRepository, JpaRepository<Mobile, Long> {
 
     @Modifying
+    @Transactional
     void deleteByClientId(String clientId);
 
     @Modifying
+    @Transactional
     @Query("""
         DELETE FROM Mobile m
         WHERE m.modifyDate < :localDateTime
