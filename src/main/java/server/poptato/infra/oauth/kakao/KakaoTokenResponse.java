1f18961b74f8cb53be8ea4693c00b8a981d7d082
신재웅 (JaeWoong Shin) <tlswodnd4316@gmail.com>
2025-11-03 02:24:54 +0900

    ILLDAN-190 [feat]: 카카오 토큰 응답 매핑용 KakaoTokenResponse를 추가한다

---
+++ src/main/java/server/poptato/infra/oauth/kakao/KakaoTokenResponse.java
@@ -0,0 +1,18 @@
+package server.poptato.infra.oauth.kakao;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record KakaoTokenResponse (
+        @JsonProperty("access_token")
+        String accessToken,
+        @JsonProperty("token_type")
+        String tokenType,
+        @JsonProperty("refresh_token")
+        String refreshToken,
+        @JsonProperty("expires_in")
+        long expiresIn,
+        String scope,
+        @JsonProperty("refresh_token_expires_in")
+        long refreshTokenExpiresIn
+) {
+}
