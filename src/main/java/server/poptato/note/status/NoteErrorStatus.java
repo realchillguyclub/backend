34b34475b3febaa9ca95275324856f3e6eb18bac
신재웅 (JaeWoong Shin) <tlswodnd4316@gmail.com>
2025-11-25 12:10:16 +0900

    ILLDAN-191 [feat]: 노트 생성시 title, content 검증을 추가한다
    - _EMPTY_TITLE_AND_CONTENT 에러코드 추가

---
+++ src/main/java/server/poptato/note/status/NoteErrorStatus.java
@@ -0,0 +1,37 @@
+package server.poptato.note.status;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import server.poptato.global.response.code.BaseErrorCode;
+import server.poptato.global.response.dto.ErrorReasonDto;
+
+@Getter
+@RequiredArgsConstructor
+public enum NoteErrorStatus implements BaseErrorCode {
+
+    _EMPTY_TITLE_AND_CONTENT(HttpStatus.BAD_REQUEST, "NOTE-001", "제목과 내용을 모두 비울 수 없습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    @Override
+    public ErrorReasonDto getReason() {
+        return ErrorReasonDto.builder()
+                .isSuccess(false)
+                .code(code)
+                .message(message)
+                .build();
+    }
+
+    @Override
+    public ErrorReasonDto getReasonHttpStatus() {
+        return ErrorReasonDto.builder()
+                .isSuccess(false)
+                .httpStatus(httpStatus)
+                .code(code)
+                .message(message)
+                .build();
+    }
+}
