4095ddbde066f98022cdaabe1962ea8a8cdf1c36
신재웅 (JaeWoong Shin) <tlswodnd4316@gmail.com>
2025-11-25 15:50:41 +0900

    ILLDAN-191 [test]: 노트 조회 테스트 시나리오를 추가한다 [SCN-SVC-NOTE-002]
    - [TC-READ-001] 노트를 정상적으로 조회한다
    - [TC-READ-EXCEPTION-001] 존재하지 않은 노트를 조회 할 경우 예외를 던진다

--- src/test/java/server/poptato/note/application/NoteServiceTest.java
+++ src/test/java/server/poptato/note/application/NoteServiceTest.java
@@ -1,20 +1,25 @@
 package server.poptato.note.application;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import server.poptato.configuration.ServiceTestConfig;
+import server.poptato.global.exception.CustomException;
 import server.poptato.note.api.request.NoteCreateRequestDto;
 import server.poptato.note.application.response.NoteCreateResponseDto;
+import server.poptato.note.application.response.NoteResponseDto;
 import server.poptato.note.domain.entity.Note;
 import server.poptato.note.domain.repository.NoteRepository;
+import server.poptato.note.status.NoteErrorStatus;
 import server.poptato.note.validator.NoteValidator;
 import server.poptato.user.validator.UserValidator;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -37,16 +42,59 @@ public class NoteServiceTest extends ServiceTestConfig {
         NoteCreateRequestDto requestDto = new NoteCreateRequestDto("title", "content");
 
         NoteValidator.validateCreate(requestDto.title(), requestDto.content());
-        Mockito.doNothing().when(userValidator).checkIsExistUser(userId);
+        doNothing().when(userValidator).checkIsExistUser(userId);
 
         Note note = mock(Note.class);
         when(note.getId()).thenReturn(1L);
         when(noteRepository.save(any(Note.class))).thenReturn(note);
 
-        //when
+        // when
         NoteCreateResponseDto responseDto = noteService.createNote(userId, requestDto);
 
-        //then
+        // then
         assertThat(responseDto.noteId()).isEqualTo(1L);
+    }
+
+    @Nested
+    @TestMethodOrder(MethodOrderer.DisplayName.class)
+    @DisplayName("[SCN-SVC-NOTE-002] 노트를 조회한다")
+    class ReadNote {
+
+
+        @Test
+        @DisplayName("[TC-READ-001] 노트를 정상적으로 조회한다")
+        void get_note_success() {
+            // given
+            Long userId = 1L;
+            Long noteId = 1L;
+            Note note = mock(Note.class);
+
+            doNothing().when(userValidator).checkIsExistUser(userId);
+            when(noteRepository.findByIdAndUserId(noteId, userId)).thenReturn(Optional.of(note));
+
+            // when
+            NoteResponseDto responseDto = noteService.getNote(noteId, userId);
+
+            // then
+            assertThat(responseDto.noteId()).isEqualTo(note.getId());
+        }
+
+
+        @Test
+        @DisplayName("[TC-READ-EXCEPTION-001] 존재하지 않은 노트를 조회 할 경우 예외를 던진다")
+        void get_invalid_note_should_throw_not_found_note() {
+            // given
+            Long userId = 1L;
+            Long noteId = 1L;
+
+            doNothing().when(userValidator).checkIsExistUser(userId);
+            when(noteRepository.findByIdAndUserId(noteId, userId)).thenReturn(Optional.empty());
+
+            // when & then
+            assertThatThrownBy(() -> noteService.getNote(noteId, userId))
+                    .isInstanceOf(CustomException.class)
+                    .satisfies(ex -> assertThat(((CustomException) ex).getHttpStatus())
+                            .isEqualTo(NoteErrorStatus._NOT_FOUND_NOTE.getHttpStatus()));;
+        }
     }
 }
