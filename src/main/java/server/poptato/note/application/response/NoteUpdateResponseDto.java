99cbaea4b60fcc1e30192c56e0c8dae987791e69
신재웅 (JaeWoong Shin) <tlswodnd4316@gmail.com>
2025-12-09 14:35:59 +0900

    ILLDAN-191 [feat]: NoteUpdateResponseDto를 생성하는 메서드를 추가한다

--- src/main/java/server/poptato/note/application/response/NoteUpdateResponseDto.java
+++ src/main/java/server/poptato/note/application/response/NoteUpdateResponseDto.java
@@ -1,8 +1,15 @@
 package server.poptato.note.application.response;
 
+import server.poptato.note.domain.entity.Note;
+
 import java.time.LocalDateTime;
 
 public record NoteUpdateResponseDto(
         Long noteId,
         LocalDateTime modifyDate
-) {}
+) {
+    public static NoteUpdateResponseDto from(Note note) {
+        return new NoteUpdateResponseDto(note.getId(), note.getModifyDate());
+    }
+
+}
