b78913ebf3cc5c0ab5513153710dfe9d4e0663bc
bbbang105 <2018111366@dgu.ac.kr>
2026-01-09 16:02:51 +0900

    remove: 불필요한 필드를 제거한다

--- src/main/java/server/poptato/app/domain/value/UpdateEventType.java
+++ src/main/java/server/poptato/app/domain/value/UpdateEventType.java
@@ -1,14 +1,7 @@
 package server.poptato.app.domain.value;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
 public enum UpdateEventType {
-    VERSION_CHECK("VERSION_CHECK"),
-    DOWNLOADED("DOWNLOADED"),
-    UPDATE_SKIPPED("UPDATE_SKIPPED");
-
-    private final String value;
+    VERSION_CHECK,
+    DOWNLOADED,
+    UPDATE_SKIPPED
 }
